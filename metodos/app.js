55e2e6f5352ad6670c6232574b296979056d75d1
asfedez <solanofedezadrian@gmail.com>
2019-07-05 08:46:14 -0600

    Ultima version

--- metodos/app.js
+++ metodos/app.js
@@ -2,7 +2,7 @@ const app = new Vue({
     el: '#app',
     data: {
 
-      
+
         activityList:[
             {id: "1", optimistic: 1, pessimistic: 3,  mostLikely: 2, name: "A", weekCost: 5000},
             {id: "2", optimistic: 2, pessimistic: 4,  mostLikely: 3, name: "B", weekCost: 20000},
@@ -10,8 +10,8 @@ const app = new Vue({
             {id: "4", optimistic: 2, pessimistic: 6,  mostLikely: 4, name: "D", dependsOn: ["2"], weekCost: 8000},
             {id: "5", optimistic: 1, pessimistic: 7,  mostLikely: 4, name: "E", dependsOn: ["3"], weekCost: 35000},
             {id: "6", optimistic: 1, pessimistic: 9,  mostLikely: 2, name: "F", dependsOn: ["3"], weekCost: 50000},
-            // {id: "7", optimistic: 3, pessimistic: 11, mostLikely: 4, name: "G", dependsOn: [4, 5], weekCost: 28000},
-            // {id: "8", optimistic: 1, pessimistic: 3,  mostLikely: 2, name: "H", dependsOn: [6, 7], weekCost: 5000},
+            {id: "7", optimistic: 3, pessimistic: 11, mostLikely: 4, name: "G", dependsOn: [4, 5], weekCost: 28000},
+            {id: "8", optimistic: 1, pessimistic: 3,  mostLikely: 2, name: "H", dependsOn: [6, 7], weekCost: 5000},
         ],
 
         indexList: null,
@@ -19,18 +19,13 @@ const app = new Vue({
         newOp:null,
         newPes:null,
         newLike:null,
-
-        //poner id en 1
-        newId: 7,
-        list:[], //Lista Pert
         dependsOn: [],
-        chart: null, //Chart Pert 
         expectTime:'',
         expectDesv:'',
         lista_Informacion: [], //Lista que obtiene datos relevantes de las tareas
         pWeekCost:'',
         projWeek:0,
-        ///
+        dataChart:[],
 
         ///Para seleccionar una dependecia
         currentDependencies: [],
@@ -39,26 +34,30 @@ const app = new Vue({
         dependencies_Options: [
             {text:'No', value:''}
         ],
-
+        
+        //Chart Pert
+        chartPert: null, 
         //Para el Diagrama GRANTT
-        dataGrantt: [],
         chartGrantt:null,
 
         //Para diagrama de barras
-        dataCost:[],
-        chartCost:null
-    },
+        chartCost:null,
+        data_LocalStorage:[],
+        report_Num:0,
+        project_Name:''
+        
+        },
     methods: {
         addActivity: function () {
-           
+
             let tempDependencies = this.currentDependencies.map(e=>{return e})
-           
+
             if(tempDependencies.length == 0){
                 tempDependencies = null
             }
 
             this.activityList.push({
-                id: this.newId++,
+                id: this.nextId,
                 name: this.newName,
                 optimistic: this.newOp,
                 pessimistic: this.newPes,
@@ -66,20 +65,12 @@ const app = new Vue({
                 dependsOn: tempDependencies,
                 weekCost: this.pWeekCost
             })
+         
 
-            this.dependencies_Options.push({
-                text: this.newName,
-                value: this.newId-1
-            })
 
-            console.log(this.selected);
-            
+            // this.saveLocalStorage()
 
-           
-            
-            
 
-            
         },
         searchActivity(id){
             for (let index = 0; index < this.activityList.length; index++) {
@@ -94,11 +85,8 @@ const app = new Vue({
                     this.indexList = index
                 }
             }
-            
         },
         editActivity(){
-                   
-                
             this.activityList[this.indexList].name=this.newName,
             this.activityList[this.indexList].optimistic= this.newOp,
             this.activityList[this.indexList].pessimistic= this.newPes,
@@ -106,61 +94,46 @@ const app = new Vue({
             this.activityList[this.indexList].dependsOn0= this.selected_Depends0,
             this.activityList[this.indexList].dependsOn1= this.selected_Depends1,
             this.activityList[this.indexList].weekCost= this.pWeekCost
-                    
-            
-
         },
         createPert(){
 
-            if(this.chart != null){
-                this.chart.dispose();
-                this.chart=null;
+            if(this.chartPert != null){
+                this.chartPert.dispose();
+                this.chartPert=null;
             }
-            
-            // create a chart
-            this.chart = anychart.pert();
-            
-            // set chart data
-            this.chart.data(this.activityList, "asTable");
-            
-            // set the title of the chart
-            this.chart.title("PERT Chart");
-            
-            // set the container id for the chart
-            this.chart.container("container");
-            
-            // initiate drawing the chart
-            this.chart.draw();  
-              
+
+            // create a chartPert
+            this.chartPert = anychart.pert();
+
+            // set chartPert data
+            this.chartPert.data(this.activityList, "asTable");
+
+            // set the title of the chartPert
+            this.chartPert.title("PERT ChartchartPert");
+
+            // set the container id for the chartPert
+            this.chartPert.container("container");
+
+            // initiate drawing the chartPert
+            this.chartPert.draw();
+
         },
         showCriticalPath(){
              // set critical path milestones colors
-            milestones = this.chart.criticalPath().milestones();
+            milestones = this.chartPert.criticalPath().milestones();
             milestones.fill("#ffab91");
             milestones.hovered().fill("#ff6e40");
             milestones.selected().fill("#ff6e40");
             milestones.labels().fontColor("#86614e");
             // set critical tasks stroke
-            tasks = this.chart.criticalPath().tasks();
+            tasks = this.chartPert.criticalPath().tasks();
             tasks.stroke("#ffab91");
         },
         showStats(){
-            
-            
-            var deviation = this.chart.getStat("pertChartCriticalPathStandardDeviation");
-            var duration = this.chart.getStat("pertChartProjectDuration");
-            
-            /*******
-             * 
-             * 
-             * 
-             * 
-             * 
-             * 
-             * 
-             * 
-             * 
-             */
+
+            var deviation = this.chartPert.getStat("pertChartCriticalPathStandardDeviation");
+            var duration = this.chartPert.getStat("pertChartProjectDuration");
+
             //Este 16 tiene que ser el tiempo de duración del proyecto
             var probabilityToEnd = (parseInt(this.projWeek)-duration)/deviation;
 
@@ -169,20 +142,19 @@ const app = new Vue({
             this.expectTime += "\n Standard deviation for this project is " + deviation.toFixed(2) + " weeks";
             this.expectTime += "\n  The probability to end the project on time is  " + probabilityToEnd.toFixed(2);
 
-
-            // this.update_Lista_Informacion();
         },
         update_Lista_Informacion(){
              //Obtener los detalles de las tareas
-             tasks = this.chart.tasks();
+             tasks = this.chartPert.tasks();
              position = 0;
 
-             let bandera = true
+             let flag = true
 
-             this.chart.tasks().lowerLabels().format(function(e){
+             this.chartPert.tasks().lowerLabels().format(function(e){
                 console.log('update_Lista_Informacion', e);
-                
-                if(bandera){
+
+                if(flag){
+                    console.log(position,  app.activityList[position],  app.activityList[position].id)
                     app.lista_Informacion[position] = {
                         id: app.activityList[position].id,
                         name: e.name,
@@ -196,19 +168,19 @@ const app = new Vue({
                     }
                     position++;
                 }
-                
-                 bandera = !bandera;
+
+                 flag = !flag;
              });
-             
+
 
              console.log(this.lista_Informacion)
- 
+
              //Ordenar la lista por orden alfabetico
              this.lista_Informacion.sort(function (a, b) {
-                 if (a.id > b.id) {
+                 if (a.earliestStart > b.earliestStart) {
                    return 1;
                  }
-                 if (a.id < b.id) {
+                 if (a.earliestStart < b.earliestStart) {
                    return -1;
                  }
                  // a must be equal to b
@@ -218,46 +190,18 @@ const app = new Vue({
         createGrantt(){
 
 
-            if(this.chartGrantt==null){
-                this.updateGranttData()
-                // create a chart
-                this.chartGrantt = anychart.bar();
-            
-                // create a range bar series and set the data
-                var series = this.chartGrantt.rangeBar(this.dataGrantt);
-            
-
-                this.chartGrantt.tooltip().titleFormat("Actividad {%x}");
-                this.chartGrantt.tooltip().format("Duración: {%duration} semanas"+
-                                        "\nInicio: semana {%low}"+
-                                        "\nFinal: semana {%high}"+
-                                        "\n{%critical}");
-
-                // set the chart title
-                this.chartGrantt.title("Range Bar Chart: Basic Sample");
-            
-                // set the titles of the axes
-                this.chartGrantt.xAxis().title("Task");
-                this.chartGrantt.yAxis().title("Weeks");
-            
-                // set the container id
-                this.chartGrantt.container("container2");
-            
-                // initiate drawing the chart
-                this.chartGrantt.draw();
-            }else{
+            if(this.chartGrantt != null){
                 this.chartGrantt.dispose();
-
                 this.chartGrantt=null;
+            }
 
-                this.updateGranttData()
-
+                this.updateChartData()
                 // create a chart
                 this.chartGrantt = anychart.bar();
-            
+
                 // create a range bar series and set the data
-                var series = this.chartGrantt.rangeBar(this.dataGrantt);
-            
+                var series = this.chartGrantt.rangeBar(this.dataChart);
+
 
                 this.chartGrantt.tooltip().titleFormat("Actividad {%x}");
                 this.chartGrantt.tooltip().format("Duración: {%duration} semanas"+
@@ -267,25 +211,24 @@ const app = new Vue({
 
                 // set the chart title
                 this.chartGrantt.title("Range Bar Chart: Basic Sample");
-            
+
                 // set the titles of the axes
                 this.chartGrantt.xAxis().title("Task");
                 this.chartGrantt.yAxis().title("Weeks");
-            
+
                 // set the container id
                 this.chartGrantt.container("container2");
-            
+                    
                 // initiate drawing the chart
                 this.chartGrantt.draw();
-            }  
         },
-        updateGranttData(){
+        updateChartData(){
             this.update_Lista_Informacion();
 
             for (let index = 0; index < this.lista_Informacion.length; index++) {
-                
-                this.dataGrantt[index] = {
-                    x: this.lista_Informacion[index].name, 
+
+                this.dataChart[index] = {
+                    x: this.lista_Informacion[index].name,
                     low: this.lista_Informacion[index].earliestStart,
                     high: this.lista_Informacion[index].earliestFinish,
                     normal: {
@@ -294,61 +237,31 @@ const app = new Vue({
                         label: {enabled: true},
                     },
                     duration: this.lista_Informacion[index].duration,
-                    critical:  (( this.lista_Informacion[index].isCritical) ? "Actividad Critica" : "No critica")
-                }     
+                    critical:  (( this.lista_Informacion[index].isCritical) ? "Actividad Critica" : "No critica"),
+                    value: this.lista_Informacion[index].weekCost*this.lista_Informacion[index].duration,
+                    weekCost: this.lista_Informacion[index].weekCost,
+                    duration: this.lista_Informacion[index].duration,
+                }
             }
         },
         costChart(){
 
-            if(this.chartCost==null){
-                this.updateCostData();
-
-                // create a chart
-                this.chartCost = anychart.column();
-
-                // create a column series and set the data
-                var series = this.chartCost.column(this.dataCost);
-
-                this.chartCost.tooltip().titleFormat("Actividad {%x}");
-                this.chartCost.tooltip().format("Costo Semanal: {%weekCost}"+
-                                    "\nDuración: {%duration} semanas"+
-                                    "\nTipo de actividad: {%critical}");
-
-
-
-                // set the this.chartCost title
-                this.chartCost.title("Costo total de las actividades");
-
-                // set the titles of the axes
-                this.chartCost.xAxis().title("Actividades");
-                this.chartCost.yAxis().title("Costo");
-
-                // set the container id
-                this.chartCost.container("container3");
-
-                // initiate drawing the this.chartCost
-                this.chartCost.draw();
-            }else{
-
-                this.updateCostData();
-
+            if(this.chartCost != null){
                 this.chartCost.dispose();
-
                 this.chartCost=null;
-
+            }
+                this.updateChartData();
 
                 // create a chart
                 this.chartCost = anychart.column();
 
                 // create a column series and set the data
-                var series = this.chartCost.column(this.dataCost);
+                var series = this.chartCost.column(this.dataChart);
 
                 this.chartCost.tooltip().titleFormat("Actividad {%x}");
                 this.chartCost.tooltip().format("Costo Semanal: {%weekCost}"+
                                     "\nDuración: {%duration} semanas"+
                                     "\nTipo de actividad: {%critical}");
-
-
 
                 // set the this.chartCost title
                 this.chartCost.title("Costo total de las actividades");
@@ -362,38 +275,109 @@ const app = new Vue({
 
                 // initiate drawing the this.chartCost
                 this.chartCost.draw();
-                
-            }
-        
-
         },
-        updateCostData(){
-            this.update_Lista_Informacion()
-            for (let index = 0; index < this.lista_Informacion.length; index++) {
-                    
-                this.dataCost[index] = {
-                    x: this.lista_Informacion[index].name,
-                    value: this.lista_Informacion[index].weekCost*this.lista_Informacion[index].duration,
-                    normal: {
-                        fill: (( this.lista_Informacion[index].isCritical) ? "#fa2050" : "#1ea8f7"),
-                        stroke: null,
-                        label: {enabled: true},
-                    },
-                    weekCost: this.lista_Informacion[index].weekCost,
-                    duration: this.lista_Informacion[index].duration,
-                    critical:  (( this.lista_Informacion[index].isCritical) ? "Critica" : "No critica")
-
-                }
-                
+        saveLocalStorage(){
+            
+            if (localStorage.report_List == null) {
+                localStorage.report_List = JSON.stringify([])
             }
+
+            let history = JSON.parse(localStorage.report_List)
+            
+            history.push( {
+                activityList: this.activityList,
+                project_Name: this.project_Name,
+                projWeek: this.projWeek 
+            })
+            
+            localStorage.report_List = JSON.stringify(history)
+        },
+        getActivityPositionById (id){
+            for (let index = 0; index < this.activityList.length; index++) {
+                let element = this.activityList[index];
+                if(element.id == id){
+                    return index
+                }
+            }
+            return -1
+        },
+        getActivityById (id) {
+            for (const activitie of this.activityList) {
+                if(activitie.id == id){
+                    return activitie
+                }
+            }
+            return null
+        },
+        getNameById (id) {
+            let act = this.getActivityById(id)
+            if(act != null){
+                return act.name
+            }
+            return null
+        },
+        deleteActivity (id) {
+
+            console.log('id', id)
+            let act = this.getActivityById(id)
+            console.log('act', act)
+
+            if(act.hasOwnProperty('dependsOn') && act.dependsOn != null){
+                for (let index = 0; index < act.dependsOn.length; index++) {
+                    let dependency = act.dependsOn[index]
+                    this.deleteActivity(dependency)
+                }
+            }
+
+            for (let index = 0; index < this.activityList.length; index++) {
+                let activity = this.activityList[index]
+                
+                if(activity.hasOwnProperty('dependsOn') && activity.dependsOn != null){
+                    let i = activity.dependsOn.indexOf(id)
+                    console.log('id', id, 'i', i, activity.name, activity)
+                    if(i > -1){
+                        activity.dependsOn.splice(i, 1)
+                    }
+                }
+            }
+
+            let position = this.getActivityPositionById(id)
+            this.activityList.splice(position, 1)
         }
-
-
-
     },
-    created() {
-        
-    },
+    computed: {
+        nextId () {
+            let list = this.activityList
+
+            let id =  parseInt( list[list.length - 1].id )
+
+            if(id > 0){
+                return id + 1
+            }else{
+                return list.length 
+            }
+        },
+        dependencies () {
+            let obj = {}
+            for (let i = 0; i < this.activityList.length; i++) {
+                let activitie = this.activityList[i]
+               
+                if(activitie.hasOwnProperty('dependsOn')){
+                    if(activitie.dependsOn != null){
+                        obj[activitie.id] = []
+
+                        for (let j = 0; j < activitie.dependsOn.length; j++) {
+                            let dependency = this.getNameById( activitie.dependsOn[j] )
+
+                            obj[activitie.id].push(dependency)
+                        }
+                    }
+                }
+            }
+
+            return obj
+        }
+    }
 })
 
 
